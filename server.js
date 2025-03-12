dd236136d3ac65e9e21dfc98f449569603ba9388
Pranavdev007 <pranavdev1999@gmail.com>
2025-03-13 02:17:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use("/api/contacts", require("./routes/contactRoutes").default);
+app.use("/api/contacts", require("./routes/contactRoutes"));
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
